3797579ade91472b0d01ac7153f88c6691c8d2a7
brendon-anderson <31911576+brendon-anderson@users.noreply.github.com>
2019-05-08 16:40:16 -0700

    Update linear_oracle_est.m

--- inverted_pendulum/linear_oracle_est.m
+++ inverted_pendulum/linear_oracle_est.m
@@ -2,9 +2,9 @@ function [ L ] = linear_oracle_est( Y,X,U )
 %LINEAR_ORACLE_EST Estimates the parameters of a linear oracle
 %   Given the n-by-(t+1) state trajectory matrix X=[x(0),x(1),...,x(t)],
 %   the m-by-(t+1) input matrix U=[u(0),u(1),...,u(t)], and the n-by-(t+1)
-%   observation matrix Y=[Y(1),Y(2),...,Y(t)], where
+%   observation matrix Y=[Y(0),Y(1),...,Y(t)], where
 %   Y(k)=x(k+1)-(A*x(k)+B*u(k)), as well as the LTI model coefficients A
-%   (n-by-n) and B (n-by-m), computes the optimal parameters F and G of the
+%   (n-by-n) and B (n-by-m), computes the optimal parameters H and G of the
 %   linear oracle of the form O(x,u)=H*x+G*u. These are returned as
 %   L={H,G}.
 
